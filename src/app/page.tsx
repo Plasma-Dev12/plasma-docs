9b652a368cfe12c487dbff881bab0d1e028e22ce
PEDRO HENRIQUE PANSTEIN <pedro_panstein@estudante.sesisenai.org.br>
2025-03-07 14:46:38 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,121 @@
+"use client";
+import Image from "next/image";
+import plasmaDocsLogo from "@/assets/Logo plasma docs.svg";
+import { FaAngleDown } from "react-icons/fa";
+import { useState } from "react";
+import GithubLoginComponent from "@/components/GithubLoginComponent";
+
+export default function Home() {
+  const options = [
+    "Introdução",
+    "Público Alvo",
+    "Objetivo do Manual",
+    "Desenvolvedores",
+  ];
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelect = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      {/* Banner inicial */}
+      <div
+        id="bgbanner"
+        className="bg-gradient-to-b from-[#28225F] to-[#0D1117] h-screen p-5 md:relative"
+      >
+        <div className="mt-10 flex flex-col justify-center items-center md:h-[350px] md:w-auto md:mx-auto lg:h-[550px] 2xl:h-[750px]">
+          <div className="text-center md:w-[500px] md:ml-[10%] xl:w-[600px] 2xl:w-[720px]">
+            <h1
+              id="title"
+              className="font-extrabold text-4xl md:text-5xl xl:text-7xl 2xl:text-8xl"
+            >
+              Plasma Docs
+            </h1>
+            <p className="text-[#FEDEFB] mt-2 text-[10px] text-center md:text-[14px] xl:text-xl xl:mt-5 2xl:text-2xl">
+              Descubra todas as funcionalidades dos seus sistemas favoritos
+            </p>
+          </div>
+          <div className="md:absolute md:left-[-180px] md:top-[60px] lg:left-[-280px] 2xl:left-[-385px]">
+            <Image
+              src={plasmaDocsLogo}
+              alt="Plasma logo"
+              id="logoPlasmaDocs"
+              className="select-none pointer-events-none w-[220px] mt-10 md:mt-0 md:w-[350px] lg:w-[550px] 2xl:w-[750px]"
+            />
+          </div>
+        </div>
+      </div>
+      {/* Introdução */}
+      <div className="flex flex-col justify-center items-center p-[32px] md:pt-[150px]">
+        <div className="max-w-[1044px] h-full w-full border-2 border-[#8C93FB] bg-[#26215b94] p-[5px] md:p-[12] lg:p-[16] rounded-[16px] shadow-[0px_0px_149.8px_-31px_#6D60C1]">
+          <div className="bg-[#0D1117] rounded-[16px] p-2 flex justify-center items-center">
+            <h1
+              id="title"
+              className="font-extrabold max-h-[349px] text-md md:text-5xl xl:text-7xl 2xl:text-8xl text-center my-[100px]"
+            >
+              {selectedOption}
+            </h1>
+          </div>
+        </div>
+        {/* Navegação Desktop */}
+        <div className="hidden md:flex justify-center items-center gap-[12px] border-1 border-[#434A53] p-[12px] rounded-[36px] mt-5">
+          {options.map((option) => (
+            <button
+              key={option}
+              className={`lg:text-[20px] lg:px-[18px] lg:py-0 lg:h-[50px] cursor-pointer border-2 font-bold p-[12px] rounded-[36px] ${
+                selectedOption === option
+                  ? "border-[#caa9ec] bg-[#252731] text-[#CCA9EC]"
+                  : "border-[#3C3C45] text-[#806C98]"
+              }`}
+              onClick={() => handleSelect(option)}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+        {/* Dropdown Mobile */}
+        <div className="relative mt-5 md:hidden">
+          <button
+            className="flex items-center justify-around gap-10 border-[1px] border-[#F6E5F8] rounded-[24px] p-[12px] text-[#CCA9EC] font-bold w-auto"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {selectedOption} <FaAngleDown className="text-[#F6E5F8]" />
+          </button>
+          {isOpen && (
+            <div className="absolute top-0 w-full bg-[#0D1117] border border-[#8C93FB] rounded-[16px] shadow-lg z-10">
+              {options.map((option) => (
+                <div
+                  key={option}
+                  className={`p-2 cursor-pointer text-center ${
+                    selectedOption === option
+                      ? "text-[#CCA9EC] font-bold underline"
+                      : "text-[#CCA9EC] opacity-[60%]"
+                  } hover:bg-[#28225F]`}
+                  onClick={() => handleSelect(option)}
+                >
+                  {option}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      {/* Pré requisitos */}
+      <h1
+        id="title"
+        className="mt-14 md:mt-32 md:mb-10 font-extrabold text-sm text-center md:text-xl xl:text-2xl 2xl:text-3xl"
+      >
+        Pré requisitos para o uso do sistema
+      </h1>
+      <div className="flex flex-col justify-center items-center p-[32px] md:pt-[-150px]">
+        <div className="max-w-[1044px] h-full w-full border-2 border-[#8C93FB] bg-[#26215b94] p-[5px] md:p-[12] lg:p-[16] rounded-[16px] shadow-[0px_0px_149.8px_-31px_#6D60C1]">
+          <GithubLoginComponent />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/GithubLoginComponent.tsx
@@ -0,0 +1,136 @@
+import { useState, useEffect } from "react";
+import { IoLogoGithub } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
+import loginDesktop from "@/assets/loginDesktop.png";
+import Image from "next/image";
+import { FaAngleDown } from "react-icons/fa";
+import { BiArrowBack, BiSearch } from "react-icons/bi";
+
+export default function GithubLoginComponent() {
+  const [showAdditionalBlock, setShowAdditionalBlock] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowAdditionalBlock((prev) => !prev);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div className="relative bg-[#040642] rounded-[16px]">
+        {/* Bloco principal com transição */}
+        <div
+          className={`transition-opacity duration-1000 ${
+            !showAdditionalBlock ? "opacity-100" : "opacity-"
+          }`}
+        >
+          <div className="md:hidden flex justify-between items-center px-[16px] py-[20px]">
+            <IoMenu className="text-[#F7F7F7] w-[32px] h-[32px]" />
+            <IoLogoGithub className="text-[#F7F7F7] w-[32px] h-[32px]" />
+            <button className="border-1 border-[#F7F7F7] text-[13px] text-[#F7F7F7] rounded-[8px] px-[11px] py-[8px]">
+              Sign in
+            </button>
+          </div>
+          <div className="hidden md:flex justify-between items-center px-[16px] py-[20px]">
+            <nav className="flex items-center gap-10">
+              <IoLogoGithub className="cursor-pointer text-[#F7F7F7] w-[32px] h-[32px]" />
+              <ul className="flex gap-4">
+                {[
+                  "Product",
+                  "Solutions",
+                  "Resources",
+                  "Open Source",
+                  "Enterprise",
+                ].map((item) => (
+                  <div
+                    key={item}
+                    className="flex items-center justify-center gap-2"
+                  >
+                    <li className="text-[8px] text-[#F7F7F7] lg:text-[12px]">
+                      {item}
+                    </li>
+                    <FaAngleDown className="cursor-pointer text-[8px] text-[#F7F7F7] lg:text-[12px]" />
+                  </div>
+                ))}
+                <li className="text-[8px] text-[#F7F7F7] lg:text-[12px]">
+                  Pricing
+                </li>
+              </ul>
+            </nav>
+            <div id="inputHeader" className="hidden relative">
+              <input
+                type="text"
+                placeholder="Search or jump to..."
+                className="border-1 pl-7 border-[#F7F7F7] text-[8px] text-[#F7F7F7] rounded-[8px] px-[11px] py-[8px] lg:w-[190px]"
+              />
+              <BiSearch className="absolute top-[9px] left-2 text-[#F7F7F7] w-[12px] h-[12px]" />
+              <button className="absolute top-[8px] text-[7px] w-[14px] h-[14px] right-2 border-1 border-[#545B66] text-[#545B66] rounded-[4px]">
+                /
+              </button>
+            </div>
+            <div className="flex gap-4">
+              <button className="text-[8px] text-[#F7F7F7] xl:text-[12px]">
+                Sign in
+              </button>
+              <button className="border-1 border-[#F7F7F7] text-[8px] text-[#F7F7F7] rounded-[8px] px-[11px] py-[8px] xl:text-[12px] transition transform hover:scale-105">
+                Sign up
+              </button>
+            </div>
+          </div>
+          <div className="md:hidden bg-gradient-to-b from-[#040642] to-[#4f3baa] px-[16px] py-[20px] rounded-b-[16px]">
+            <h2 className="text-3xl font-bold md:text-4xl text-center text-[#F7F7F7]">
+              Build and ship software on a single, collaborative platform
+            </h2>
+            <p className="mt-4 text-md text-[#F7F7F7] text-center">
+              Join the world’s most widely adopted AI-powered developer
+              platform.
+            </p>
+
+            <div className="mt-6 flex flex-col md:flex-row bg-[#F7F7F7] rounded-[8px] p-1">
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className="w-full md:w-80 px-4 py-3 rounded-md text-[#535558] font-bold outline-none"
+              />
+              <button className="bg-green-600 hover:bg-green-500 transition px-6 py-3 rounded-md font-semibold text-white">
+                Sign up for GitHub
+              </button>
+            </div>
+
+            <button className="w-full mt-4 px-6 py-3 rounded-md font-semibold bg-gray-800/10 border-2 border-[#F7F7F7] text-white hover:bg-gray-700 transition">
+              Try GitHub Copilot
+            </button>
+          </div>
+          <Image
+            src={loginDesktop}
+            alt="loginMobile"
+            className="select-none pointer-events-none hidden md:block w-full h-full object-contain"
+          />
+        </div>
+
+        {/* Bloco adicional com transição */}
+        <div
+          className={`absolute w-full h-full inset-0 bg-black/80 flex items-start pt-[45px] pr-[23px] justify-end rounded-[16px] transition-opacity duration-1000 ${
+            showAdditionalBlock ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <button className="md:hidden relative border-2 border-[#F7F7F7] text-[18px] bg-[#02031A] text-[#F7F7F7] rounded-[8px] px-[11px] py-[8px] transition transform hover:scale-105">
+            Sign in
+          </button>
+          <button className="md:block hidden relative border-2 border-[#F7F7F7] text-[18px] bg-[#02031A] text-[#F7F7F7] rounded-[8px] px-[11px] py-[8px] transition transform hover:scale-105">
+            Sign up
+          </button>
+          <div className="absolute border-4 top-[10px] right-[10px] border-red-700 rounded-full w-[120px] h-[120px]"></div>
+          <BiArrowBack className="absolute text-red-700 text-[120px] top-[80px] right-[120px] rotate-[145deg]" />
+          <h3 className="absolute top-[45%] mx-10 right-0 left-0 font-extrabold text-sm text-center md:text-2xl xl:text-3xl 2xl:text-4xl text-[#F7F7F7]">
+            Para começar abra o site{" "}
+            <span className="text-blue-300">github.com</span> no seu navegador e
+            clique em <span className="text-purple-300">sign up</span>
+          </h3>
+        </div>
+      </div>
+    </>
+  );
+}
