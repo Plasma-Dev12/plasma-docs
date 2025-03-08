9f69465eab355f9235993493998008d3c62be394
Pedro Henrique Panstein <phfarof@gmail.com>
2025-03-08 01:25:22 -0300

    :recycle: refactor: Text of the objectives component of the manual changed and finalized

--- src/components/ObjectivesComponent.tsx
+++ src/components/ObjectivesComponent.tsx
@@ -11,10 +11,12 @@ export default function ObjectivesComponent() {
           alt="Octocat Objective"
         />
         <p className="font-normal text-[#FEDEFB] text-[14px] text-justify max-w-[350px] md:max-w-[300px] md:text-xl lg:max-w-[400px]">
-          Esta documentação tem como objetivo fazer você entender sobre as
-          principais funcionalidades e características do{" "}
-          <span className="font-bold text-[#A486FF]">github</span>, te ensinado
-          como utilizar a plataforma da melhor maneira possível.
+          Esta documentação tem como objetivo proporcionar um entendimento sobre
+          as principais funcionalidades e características do{" "}
+          <span className="font-bold text-[#A486FF]">GITHUB</span>. Aqui, você
+          aprenderá a utilizar a plataforma de forma eficiente, aproveitando
+          seus recursos para versionamento de código, colaboração e automação de
+          tarefas, além de boas práticas para otimizar seu fluxo de trabalho.
         </p>
       </div>
     </>
