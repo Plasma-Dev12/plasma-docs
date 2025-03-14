9d75121265f7ec6719fa4d4577ff8ef3962d673a
ARTUR NEVES HOPNER <artur_hopner@estudante.sesisenai.org.br>
2025-03-13 21:49:22 -0300

    created erroSolucao, dica and started atalho

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,11 +1,19 @@
+import Dica from "@/components/Dica";
+import dicas from "@/content/dicas.json";
 export default function GithubDocs() {
   return (
     <>
-      <div>
-        <h1 id="title" className="text-[5rem] font-bold">
-          Hello world!
-        </h1>
-      </div>
+      <main className="flex flex-row w-screen">
+        <section className="w-[300px] bg-slate-950"></section>
+        <section className="mx-auto w-3/5 overflow-y-auto">
+          <div className="grid grid-cols-3 gap-8 items-stretch p-4 justify-center w-full ">
+            {dicas.map((dica, index) => (
+              <Dica key={index} content={dica} />
+            ))}
+            {/* <ErrosSolucoes content={errosSolucoes[0]} /> */}
+          </div>
+        </section>
+      </main>
     </>
   );
 }
